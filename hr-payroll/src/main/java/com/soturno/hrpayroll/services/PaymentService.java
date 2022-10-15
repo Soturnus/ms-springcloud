3971d4bf0a054c6f111f0f0d64fbc73e44caa08a
Soturnus <rivaldo.valhalla@gmail.com>
2022-10-15 19:50:26 -0300

    Adicionando Feign Client

--- hr-payroll/src/main/java/com/soturno/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/soturno/hrpayroll/services/PaymentService.java
@@ -1,33 +1,22 @@
 package com.soturno.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.soturno.hrpayroll.entities.Payment;
 import com.soturno.hrpayroll.entities.Worker;
+import com.soturno.hrpayroll.feingclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days ) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
-		String workId = Long.toString(workerId);
-		uriVariables.put("id", ""+workId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables); 
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
