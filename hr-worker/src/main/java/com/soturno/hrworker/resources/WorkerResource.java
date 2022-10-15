ac31d52fe43d5388448e4866335ee149001bb4f3
Soturnus <rivaldo.valhalla@gmail.com>
2022-10-15 20:18:02 -0300

    implementando Ribbon para loadBalance

--- hr-worker/src/main/java/com/soturno/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/soturno/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.soturno.hrworker.resources;
 
 import java.util.List;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +19,11 @@ import com.soturno.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -30,8 +39,9 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
-		Worker worker = workerRepository.findById(id).get();
+		logger.info("PORT = {}", env.getProperty("local.server.port"));
 		
+		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
 }
