c5649c9673c7ee6e4236a139a872115cf6dd6cad
Soturnus <rivaldo.valhalla@gmail.com>
2022-10-18 05:45:33 -0300

    criando login e rota para token jwt

--- hr-oauth/src/main/java/com/soturno/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/soturno/hroauth/services/UserService.java
@@ -3,21 +3,34 @@ package com.soturno.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.soturno.hroauth.entities.User;
 import com.soturno.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private UserFeignClient userFeignClient;
-	
-	public User findByEmail(String email) {
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: {}", email);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: {}", email);
+		return user;
+	}
 		
+	public User findByEmail(String email) {		
 		User user = userFeignClient.findByEmail(email).getBody();
 		if (user == null) {
 			logger.error("Email not found: {}", email);
@@ -26,5 +39,4 @@ public class UserService {
 		logger.info("Email found: {}", email);
 		return user;
 	}
-	
 }
